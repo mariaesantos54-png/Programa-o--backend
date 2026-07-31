0d8bac9cb8211ec4d16972720ecbe0912c283b52
Maria Pereira <maria.e.santos54@edu.senai.br>
2026-07-31 15:37:00 -0300

    feat: adiciona atividade minha estante

--- 01-ambiente-node/Atividade-01/estante.js
+++ 01-ambiente-node/Atividade-01/estante.js
@@ -12,5 +12,12 @@ autor: "Ali Hazelwood",
 preco: 65.00,
 estoque: 10,
 };
+
+const livro3 = {
+titulo: "Estilhaça-me",
+autor: "Tahereh Mafi",
+preco: 216.04,
+estoque: 5,
+};
 // Esta linha entrega os dois livros para os outros arquivos usarem
-module.exports = { livro1, livro2 };
+module.exports = { livro1, livro2, livro3};
--- 01-ambiente-node/Atividade-01/mostrar.js
+++ 01-ambiente-node/Atividade-01/mostrar.js
@@ -11,14 +11,26 @@ console.log("Preco: R$ " + estante.livro1.preco);
 console.log("Estoque: " + estante.livro1.estoque + " unidades");
 console.log("");
 
-// ===== LIVRO 2 — complete as 3 lacunas =====
+console.log("-----------------------------------");
+
 console.log("Titulo: " + estante.livro2.titulo);
-// LACUNA 1 → troque _____ pelo nome do objeto do segundo livro
 console.log("Autor: " + estante.livro2.autor);
-// LACUNA 2 → troque _____ pelo nome da propriedade do preço
 console.log("Preco: R$ " + estante.livro2.preco);
-// LACUNA 3 → copie aqui a linha do "Estoque:" do Livro 1
-// e troque livro1 por livro2
+
+console.log("-----------------------------------");
+
+console.log("Titulo: " + estante.livro3.titulo);
+console.log("Autor: " + estante.livro3.autor);
+console.log("Preco: R$ " + estante.livro3.preco);
+console.log("Estoque: " + estante.livro3.estoque + " unidades");
+console.log("");
+
+console.log("-----------------------------------");
+
 console.log("Estoque: " + estante.livro2.estoque + " unidades");
+console.log("Total de unidades: " + (estante.livro1.estoque + estante.livro2.estoque + estante.livro3.estoque),);
+console.log("Valor em estoque do livro 1: R$ " +estante.livro1.preco * estante.livro1.estoque,);
+console.log("Valor em estoque do livro 2: R$ " +estante.livro2.preco * estante.livro2.estoque,);
+console.log("Valor em estoque do livro 3: R$ " +estante.livro3.preco * estante.livro3.estoque,);
 console.log("");
 console.log("===================================");
